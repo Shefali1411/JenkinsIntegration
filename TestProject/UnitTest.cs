18361a7e48f95d173dfd7243124ce3d08feb1a7f
Shefali Gandhi <sgandhi@redtechnologies.fr>
2018-05-14 11:51:41 +0530

    Add project files.

---
+++ TestProject/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsIntegrationTest.Controllers;
+using System.Web.Mvc;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Should_Get_Index()
+        {
+            HomeController homeController = new HomeController();
+            ViewResult result = homeController.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
